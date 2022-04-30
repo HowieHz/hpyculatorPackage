4778f88e60d5ae8a27e0ec5a800beb2845720a19
HowieHz <94725606+HowieHz@users.noreply.github.com>
2022-04-30 19:17:24 +0800

    1.2.0
    更新api

--- hpyculator/__init__.py
+++ hpyculator/__init__.py
@@ -27,20 +27,17 @@ def setOutput(self, msg:str) -> None:
 def addOne(num:int) -> int:
     return num+1
 
-# def band():
-#     main_window_signal.appendOutPutBox.connect(appendOutPut)
+STRING=(1<<0)
+NUM=(1<<1)
+FLOAT=(1<<2)
+LIST=(1<<3)
 
-#     main_window_signal.setOutPutBox.connect(setOutPut)
+ON=(1<<1)
+OFF=(1<<0)#因为没读到就用这个作为缺省，所以每个参数的(1<<0)就是缺省
 
-#     main_window_signal.clearOutPutBox.connect(clearOutPut)
-
-# def appendOutPut(self, msg:str):
-#     self.ui.output_box.appendPlainText(msg)
-
-# def clearOutPut(self):
-#     self.ui.output_box.clear()
-
-# def setOutPut(self, msg:str):
-#     self.ui.output_box.setPlainText(msg)
-
-# band()
+#'output_mode'
+RETURN_ONCE=(1<<0)
+RETURN_LIST=(1<<1)
+RETURN_LIST_OUTPUT_IN_ONE_LINE=(1<<2)
+NO_RETURN=(1<<3)
+NO_RETURN_SINGLE_FUNCTION=(1<<4)
--- setup.py
+++ setup.py
@@ -7,13 +7,13 @@ with open("README.md", "r") as fh:
 setup(
     # 以下为必需参数
     name='hpyculator',
-    version='1.1.1',
+    version='1.2.0',
     description='这是一个基于python的可编程计算器',
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
     long_description=long_description,
-    url='https://github.com/pypa/sampleproject',
+    url='https://github.com/HowieHz/hpyculatorPackage',
     author='HowieHz', # 作者名
     classifiers=[
         'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
@@ -22,7 +22,6 @@ setup(
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
 
     ],
     install_requires=[

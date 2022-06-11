f1f85a55451cb08bba43f39d5e60bac8fe014096
HowieHz <94725606+HowieHz@users.noreply.github.com>
2022-06-11 19:44:22 +0800

    修改reRunTimes和funName的注解，修改reRunTimes
    为了准确性将time.perf_counter()替换成time.perf_counter_ns()

--- src/hpyculator/hpydecorator.py
+++ src/hpyculator/hpydecorator.py
@@ -6,40 +6,41 @@ def reRunTimes(times: int = 1) -> Callable:
     """
     一个装饰器，用来计算函数运行时长，这个函数是装饰器参数
 
+    ---------------------------------------------
+
+    返回一个元组：(原函数调用结果，ns为单位的运行时长)
+
     :param times: 运行次数，默认为1
-    :return: 一个元组，第一项为函数的返回值，第二项为函数运行时长
+    :return Tuple: 原函数调用结果，ns为单位的运行时长
     """
-
     def ruturnFun(fun: Callable) -> Callable:
-        """
-        装饰器本体
-
-        :param fun: 要装饰的函数
-        :return: 函数
-        """
-
-        def runFun(*args, **kwargs) -> Tuple[Callable, float]:
-            """
-            装饰器
-
-            :param args: 参
-            :param kwargs: 形参
-            :return: 函数
-            """
-            _time_start = time.perf_counter()
+        """nothing"""
+        def runFun(*args, **kwargs) -> Tuple[Callable, int]:
+            """nothing"""
+            _time_start = time.perf_counter_ns()
             for _ in range(times):
                 fun_ret = fun(*args, **kwargs)
-            time_used = time.perf_counter() - _time_start
+            time_used = time.perf_counter_ns() - _time_start
             return fun_ret, time_used
-
         return runFun
-
     return ruturnFun
 
 
 def funName(fun: Callable) -> Callable:
     """
-    一个装饰器，函数形参增加__name__用于获得函数名
+    一个装饰器，函数形参增加__fun_name__用于获得函数名
+
+    ---------------------------------------------
+
+    使用方法：
+
+    原函数
+        def a(c, b: int = 1):
+
+    ->
+
+    改成
+        def a(c, b: int = 1, __fun_name__):
 
     :param fun: 一个函数
     :return: 一个函数

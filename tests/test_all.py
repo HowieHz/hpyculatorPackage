bb5ec5cabae0cfa981535561b47c975037ef6a7d
HowieHz <94725606+HowieHz@users.noreply.github.com>
2022-06-25 14:38:03 +0800

    Update test_all.py

--- tests/test_all.py
+++ tests/test_all.py
@@ -3,8 +3,8 @@ from random import randint
 from typing import IO, Any, AnyStr
 
 import pytest
-from hpyc import hpycore, hpydecorator, hpyfunc
-from hpyc.hpysignal import instance_main_win_signal as main_signal
+from .hpyc import hpycore, hpydecorator, hpyfunc
+from .hpyc.hpysignal import instance_main_win_signal as main_signal
 
 # # 这样才可以导入上层包哈哈
 # sys.path.append(os.path.join(sys.path[0], ".."))

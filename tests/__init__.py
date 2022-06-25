a581dcace3730bc2f65691cb94dee928d743cf25
HowieHz <94725606+HowieHz@users.noreply.github.com>
2022-06-25 14:25:11 +0800

    更新test

---
+++ tests/__init__.py
@@ -0,0 +1,1 @@
+import hpyculator as hpyc
--- tests/test_all.py
+++ tests/test_all.py
@@ -6,8 +6,8 @@ import pytest
 # # 这样才可以导入上层包哈哈
 # sys.path.append(os.path.join(sys.path[0], ".."))
 
-from .hpyculator import hpycore, hpydecorator, hpyfunc
-from .hpyculator.hpysignal import instance_main_win_signal as main_signal
+from hpyc import hpycore, hpydecorator, hpyfunc
+from hpyc.hpysignal import instance_main_win_signal as main_signal
 from random import randint
 
 test_buffer: Any = 0  # 初始化一个变量，用于检测结果

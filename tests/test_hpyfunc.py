158fe6ee2a574fec5eb5ccf1dcbb4d0b9446b362
HowieHz <94725606+HowieHz@users.noreply.github.com>
2022-07-21 15:50:20 +0800

    1.5.3
    1.5.3
    
    Added
            添加hpyfunc.expand_dims

--- tests/test_hpyfunc.py
+++ tests/test_hpyfunc.py
@@ -6,3 +6,4 @@ def test_hpyfunc():
     assert [1, 2, 3, 4, 5, 6, 7] == hpyfunc.flatten(test_list)
     assert [1, 2, 3, 4, 5, [6, 7]] == hpyfunc.flatten_layer(test_list)
     assert [1, 2, 3, 4, 5, 6, 7] == hpyfunc.flatten_no_recursion(test_list)
+    assert [[1, 2], [3, 4]] == hpyfunc.expand_dims([1, 2, 3, 4], 2, 2)

b2af4552dc06b6d596678be20bf91552a1abe925
HowieHz <94725606+HowieHz@users.noreply.github.com>
2022-07-21 15:50:20 +0800

    1.5.3
    Added
            添加hpyfunc.expand_dims

--- hpyculator/hpyfunc.py
+++ hpyculator/hpyfunc.py
@@ -1,4 +1,5 @@
 from typing import Union
+from array import ArrayType
 
 
 def flatten(sequence: Union[list, tuple]) -> list:
@@ -40,3 +41,23 @@ def flatten_no_recursion(sequence: Union[list, tuple]) -> Union[list, tuple]:
         if all(not isinstance(_, list) for _ in sequence):  # 全不是列表
             return sequence
         sequence = flatten_layer(sequence)
+
+
+def expand_dims(
+    array: Union[ArrayType, list], *dims, padding_value: Union[str, int, float] = 0
+):
+    """将一纬数据结构提升至多维
+
+    :param array: 一维列表/数组
+    :param dims: 各项维度上限 (顺序从最里层向外)
+    :param padding_value: 填充值 默认为0
+    """
+    allLen: int = 1  # 总长
+    for num in dims:
+        allLen *= num
+    if len(array) < allLen:  # 补齐长度
+        array.extend([padding_value for _ in range(allLen - len(array))])
+
+    for n in dims:
+        array = [array[i : i + n] for i in range(0, len(array), n)]
+    return array[0]
